6d43a5232ed733a4afa8cbfdcff74f546cc0a88e
KiriLL228x <100783309+KiriLL228x@users.noreply.github.com>
2022-03-18 20:35:15 +0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,11 +1,26 @@
 #include "TXLib.h"
+struct Key
+{
+int x;
+int y;
+HDC picture;
+bool visible;
+};
+struct Spikes
+{
+int x;
+int y;
+HDC picture;
 
+};
 struct Moneta
 {
 int x;
 int y;
-HDC picture
+HDC picture;
+bool visible;
 };
+
 struct Mario
 {
  int x;
@@ -30,7 +45,30 @@ void drawblock(Block block)
     txSetFillColor(TX_ORANGE);
 
     txRectangle(block.x,block.y,block.x+block.w,block.y+10);
-};
+}
+
+void drawKey(Key key)
+{
+if(key.visible)
+    {
+        txTransparentBlt (txDC(), key.x, key.y, 25, 25, key.picture, 0, 0, TX_WHITE);
+    }
+}
+
+void drawSpikes(Spikes spikes)
+{
+    txTransparentBlt (txDC(), spikes.x, spikes.y, 100, 100, spikes.picture, 0, 0, TX_WHITE);
+}
+
+void drawMoneta(Moneta moneta)
+{
+
+
+if(moneta.visible)
+    {
+        txTransparentBlt (txDC(), moneta.x,moneta.y,31,29,moneta.picture,0,0,TX_WHITE);
+    }
+}
 
 
 int main()
@@ -41,17 +79,31 @@ int main()
     HDC marios = txLoadImage ("картинки/марио/runMarioRight.bmp");
     HDC marioright = marios;
     HDC marioleft = txLoadImage ("картинки/марио/runMarioLeft.bmp");
-    HDC moneta = txLoadImage ("картинки/предметы/moneta.bmp");
-    Mario mario{350,750,0,marios,marioright,marioleft, 750};
-    Moneta moneta{450,750};
+
+    Mario mario = {350,750,0,marios,marioright,marioleft, 750};
+
     int kolm=0;
+
     int Kolblock = 3;
     Block block[100];
-    block[0] = {450, 550, 100};
+    block[0] = {450, 550, 200};
     block[1] = {650, 550, 100};
     block[2] = {850, 400, 100};
-
     char strmario[100];
+
+    int Kolmoneta = 3;
+    Moneta moneta[100];
+    moneta[0] = {450, 750, txLoadImage ("картинки/предметы/moneta.bmp"), true};
+
+    int kolkey = 1;
+    Key key[100];
+    key[0] = {650, 750, txLoadImage ("картинки/предметы/key.bmp"), true};
+
+    int kolspikes = 3;
+    Spikes spikes[100];
+    spikes[0] = {450,450, txLoadImage ("картинки/предметы/Spikes.bmp")};
+    spikes[1] = {1150,750, txLoadImage ("картинки/предметы/Spikes.bmp")};
+    spikes[2] = {1350,750, txLoadImage ("картинки/предметы/Spikes.bmp")};
 
 while(!GetAsyncKeyState(VK_ESCAPE))
 {
@@ -62,8 +114,26 @@ while(!GetAsyncKeyState(VK_ESCAPE))
     drawblock(block[i]);
     }
 
+    for(int i=0; i<Kolmoneta; i++)
+    {
+    drawMoneta(moneta[i]);
+    }
+
+    for(int i=0; i<kolkey; i++)
+    {
+    drawKey(key[i]);
+    }
+
+    for(int i=0; i<kolspikes; i++)
+    {
+    drawSpikes(spikes[i]);
+    }
+
     txSetFillColor(TX_BLACK);
-    txTransparentBlt (txDC(), moneta.x,moneta.y,35,38,moneta,0,0,0);
+
+
+
+
     txTransparentBlt (txDC(), mario.x,mario.y,92,100,mario.picture,0,0, TX_WHITE);
 
     if(GetAsyncKeyState('D'))
@@ -71,18 +141,15 @@ while(!GetAsyncKeyState(VK_ESCAPE))
         mario.x=mario.x+10;
         mario.picture=mario.right;
     }
-
-
     if(GetAsyncKeyState('A'))
     {
         mario.x=mario.x-10;
         mario.picture=mario.left;
     }
-
     if(GetAsyncKeyState(VK_SPACE))
     {
         while(GetAsyncKeyState (VK_SPACE))
-        txSleep(1);
+        txSleep(50);
         mario.vy=-30;
     }
 
@@ -119,13 +186,37 @@ while(!GetAsyncKeyState(VK_ESCAPE))
        !(mario.x>block[2].x-block[2].w && mario.x<block[2].x+block[2].w))
         mario.floor = 750;
 
-    if(mario.x==450 and mario.y==750)
-        visible
-        kolm=kolm+1;
 
+//система монет
+    for(int i=0; i<Kolmoneta; i++)
+    {
+        if(mario.x==moneta[i].x && mario.y==moneta[i].y && moneta[i].visible)
+        {
+            moneta[i].visible = false;
+            kolm=kolm+1;
+        }
+    }
 
+//ключи
+    for(int i=0; i<kolkey; i++)
+    {
+        if(mario.x==key[i].x && mario.y==key[i].y && key[i].visible)
+        {
+            key[i].visible = false;
+        }
+    }
 
-txSleep(10);
+//механика шипов
+for(int i=0; i<kolspikes; i++)
+    {
+        if(mario.x==spikes[i].x && mario.y==spikes[i].y)
+        {
+        txTextOut(900, 450,"GAME OVER");
+        }
+    }
+
+txSleep(15);
+txEnd();
 }
 
 
